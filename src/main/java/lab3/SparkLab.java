183330ef96d0a64cb7da006276db1bf6830bd5f5
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-17 12:18:04 +0300

    Scripted auto-commit on change (2019-10-17 12:18:04) by gitwatch.sh

--- src/main/java/lab3/SparkLab.java
+++ src/main/java/lab3/SparkLab.java
@@ -11,6 +11,6 @@ public class SparkLab {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> flightsFile = sc.textFile();
+        JavaRDD<String> flightsFile = sc.textFile("664600583_T_ONTIME_sample.csv");
     }
 }
