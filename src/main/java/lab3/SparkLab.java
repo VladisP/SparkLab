2724ba68d8279cc1d3a16187e99db70c7865646c
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-17 12:21:03 +0300

    Scripted auto-commit on change (2019-10-17 12:21:03) by gitwatch.sh

--- src/main/java/lab3/SparkLab.java
+++ src/main/java/lab3/SparkLab.java
@@ -4,6 +4,8 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import java.util.Arrays;
+
 public class SparkLab {
 
     public static void main(String[] args) {
@@ -12,6 +14,8 @@ public class SparkLab {
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> flightsFile = sc.textFile("664600583_T_ONTIME_sample.csv");
-        JavaRDD<String> splitted = flightsFile.flatMap(s -> s.replaceAll("\"", "").split(","));
+        JavaRDD<String> splitted = flightsFile.flatMap(s -> Arrays.stream(
+                s.replaceAll("\"", "").split(",")
+        ).iterator());
     }
 }
