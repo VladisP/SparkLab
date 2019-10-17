e32abca1b6de1a8ffa6394b8b14ec855c6c8caec
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-17 12:47:54 +0300

    Scripted auto-commit on change (2019-10-17 12:47:54) by gitwatch.sh

--- src/main/java/lab3/SparkLab.java
+++ src/main/java/lab3/SparkLab.java
@@ -26,6 +26,8 @@ public class SparkLab {
         JavaRDD<String[]> usefulFlightsColumns = flightsColumns.filter(
                 arr -> !arr[DEST_AIRPORT_ID_COLUMN].equals(DEST_ID_HEAD_VALUE)
         );
-        JavaPairRDD<Tuple2<Integer, Integer>, FlightStatistics>
+        JavaPairRDD<Tuple2<Integer, Integer>, FlightStatistics> flightsStatisticsPairs = usefulFlightsColumns.mapToPair(
+                arr -> new JavaPairRDD<>()
+        );
     }
 }
