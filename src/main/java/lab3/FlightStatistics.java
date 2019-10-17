f669bbc12df0260e3bcacc43234e29bea083b49a
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-17 13:41:29 +0300

    Scripted auto-commit on change (2019-10-17 13:41:29) by gitwatch.sh

--- src/main/java/lab3/FlightStatistics.java
+++ src/main/java/lab3/FlightStatistics.java
@@ -9,6 +9,22 @@ public class FlightStatistics implements Serializable {
     private long cancelledFlightsCount;
     private long flightsCount;
 
+    public float getMaxDelayTime() {
+        return maxDelayTime;
+    }
+
+    public long getLateFlightsCount() {
+        return lateFlightsCount;
+    }
+
+    public long getCancelledFlightsCount() {
+        return cancelledFlightsCount;
+    }
+
+    public long getFlightsCount() {
+        return flightsCount;
+    }
+
     FlightStatistics(float maxDelayTime, long lateFlightsCount, long cancelledFlightsCount, long flightsCount) {
         this.maxDelayTime = maxDelayTime;
         this.lateFlightsCount = lateFlightsCount;
@@ -17,6 +33,11 @@ public class FlightStatistics implements Serializable {
     }
 
     static FlightStatistics union(FlightStatistics statistics1, FlightStatistics statistics2) {
-        return new FlightStatistics(Math.max(statistics1.maxDelayTime, statistics2.maxDelayTime));
+        return new FlightStatistics(
+                Math.max(statistics1.getMaxDelayTime(), statistics2.getMaxDelayTime()),
+                statistics1.getLateFlightsCount() + statistics2.getLateFlightsCount(),
+                statistics1.getCancelledFlightsCount() + statistics2.getCancelledFlightsCount(),
+                statistics1.getFlightsCount() + statistics2.getFlightsCount()
+        );
     }
 }
