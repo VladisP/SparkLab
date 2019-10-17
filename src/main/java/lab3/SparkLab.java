b4a9a42a3138255b5ead42c7dbf9de616b09de9e
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-17 12:15:03 +0300

    Scripted auto-commit on change (2019-10-17 12:15:03) by gitwatch.sh

--- src/main/java/lab3/SparkLab.java
+++ src/main/java/lab3/SparkLab.java
@@ -1,11 +1,13 @@
 package lab3;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 
 public class SparkLab {
 
     public static void main(String[] args) {
 
         SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext 
     }
 }
