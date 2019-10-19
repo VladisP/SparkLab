165210e551393703e8483d05f84967fca0ed4fab
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-19 12:24:02 +0300

    Scripted auto-commit on change (2019-10-19 12:24:02) by gitwatch.sh

--- src/main/java/lab3/CsvParser.java
+++ src/main/java/lab3/CsvParser.java
@@ -1,4 +1,11 @@
 package lab3;
 
+import org.apache.hadoop.io.Text;
+
 public class CsvParser {
+
+    public static String[] getColumns(Text row) {
+
+        return row.toString().replaceAll("\"", "").split(",");
+    }
 }
